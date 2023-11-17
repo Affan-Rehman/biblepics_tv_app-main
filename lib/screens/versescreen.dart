ae0a5e819e6b83252d619c61c1cb83907842fa30
Affan Rehman <112381046+Affan-Rehman@users.noreply.github.com>
2023-11-17 20:13:05 +0500

    .

--- lib/screens/versescreen.dart
+++ lib/screens/versescreen.dart
@@ -233,44 +233,45 @@ class _VerseScreenState extends State<VerseScreen> {
       child: Stack(
         alignment: Alignment.center,
         children: [
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                if (!verses[index].imageLoadFailed)
-                  Container(
-                    width: MediaQuery.of(context).size.height * 0.65,
-                    height: MediaQuery.of(context).size.height * 0.65,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(12.0),
-                      child: Image.network(verses[index].imageurl,
-                          fit: BoxFit.cover, errorBuilder:
-                              (context, error, StackTrace? stackTrace) {
-                        _handleImageLoadFailure(index);
-                        return SizedBox();
-                      }),
-                    ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+              if (verses[index].imagefound)
+                Container(
+                  width: MediaQuery.of(context).size.height * 0.6,
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
                   ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  child: Text(
-                    verses[index].versetext,
-                    style: TextStyle(
-                      fontFamily: "Cardo",
-                      color: Colors.white.withOpacity(0.7),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12.0),
+                    child: Image.network(
+                      verses[index].imageurl,
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-                const SizedBox(height: 5),
-              ],
-            ),
+              SizedBox(
+                height: verses[index].imagefound
+                    ? 10
+                    : MediaQuery.of(context).size.height * 0.3,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                alignment: Alignment.center,
+                child: Text(
+                  verses[index].versetext,
+                  style: TextStyle(
+                    fontFamily: "Cardo",
+                    color: Colors.white.withOpacity(0.7),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
+                ),
+              ),
+            ],
           ),
           Positioned(
             bottom: 0,
